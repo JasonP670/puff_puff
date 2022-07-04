148b06a9bec4f505f6585c5841f05aa524f51702
Jason <jasonp670@gmail.com>
2022-07-04 11:47:26 -0600

    tweak getData to production vs development

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,11 @@
-export async function getData(url = 'https://jasonp670-puffs.herokuapp.com/api/v1/', body = '') {
-  const response = await fetch(url, {
-    body: body,
-  });
+export async function getData(resource = '') {
+  let url;
+  if (process.env.NODE_ENV === 'production') {
+    url = `https://jasonp670-puffs.herokuapp.com/api/v1/${resource}`;
+  } else {
+    url = `http://localhost:3001/api/v1/${resource}`;
+  }
+  const response = await fetch(url);
   const data = await response.json();
   return data;
 }
